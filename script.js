ae5e402f39b15a22c17380b5a588df33fadd4cfb
Morgone12 <salazarjhorman818@gmail.com>
2024-04-23 10:30:47 -0500

    #

---
+++ script.js
@@ -0,0 +1,12 @@
+let Persona = {
+    name: 'jhorman',
+    aprellido: 'salazar',
+    padre: {
+        namep: 'Rolando',
+        apellodp: 'Salazar G',
+        edad:51
+    },
+    hobby: 'tocar guitarra',
+};
+
+console.log(Persona);
